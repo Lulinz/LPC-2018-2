360299cfba2d3b8ed4ae8b4b4bc78b80dbb93387
Cássio Costa <cassio@bitfacil.com>
2018-10-26 19:23:24 -0300

    Aula 14 - template para api restful

---
+++ Aula14 - api rest/ToDoList.Domain/ToDo.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace ToDoList.Domain
+{
+    public class ToDo
+    {
+        public int id { get; set; }
+        public string descricao { get; set; }
+    }
+}
---
+++ Aula14 - api rest/ToDoList.Repositories/Migrations/20181019162732_initial migrations.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace ToDoList.Repositories.Migrations
+{
+    public partial class initialmigrations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Todos",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    descricao = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Todos", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Todos");
+        }
+    }
+}
